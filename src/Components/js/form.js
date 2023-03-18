576cb9a223be97cf399a2a73ccb5fdc588431fc5
Abdallah.S <109352054+SnakeEyee@users.noreply.github.com>
2023-03-18 23:31:29 +0200

    new

--- src/Components/js/form.js
+++ src/Components/js/form.js
@@ -2,12 +2,19 @@ import { useState, useRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { firestore } from "./firebase";
 import { addDoc, collection } from "firebase/firestore";
-import "../css/style.css";
+import Select from "react-select";
+import "../css/form.css";
 
 const Form = () => {
   const [verify, setVerify] = useState(false);
 
   const [selectedOption, setSelectedOption] = useState();
+
+  const branch_options = [
+    { value: "antelias", label: "Antelias" },
+    { value: "badaro", label: "Badaro" },
+    { value: "tripoli", label: "Tripoli" },
+  ];
 
   const first_name = useRef(null);
   const last_name = useRef(null);
@@ -46,60 +53,67 @@ const Form = () => {
 
   return (
     <div className="container">
-      <form onSubmit={handleSubmit}>
-        <h1 className="text">Tech Fusion Club</h1>
-        <div className="box">
-          <div className="form">
-            <h2>Fill The Form</h2>
-            <div className="inputBox">
-              <input type="text" required="required" ref={first_name} />
-              <span>First Name</span>
-              <i></i>
-            </div>
-            <div className="inputBox">
-              <input type="text" required="required" ref={last_name} />
-              <span>Last Name</span>
-              <i></i>
-            </div>
-            <div className="inputBox">
-              <input type="text" required="required" ref={short_id} />
-              <span>Short ID</span>
-              <i></i>
-            </div>
-            <div className="inputBox">
-              <input type="text" required="required" ref={mail} />
-              <span>User ID</span>
-              <i></i>
-            </div>
-            <div className="inputBox">
-              <input type="tel" required="required" ref={phone} />
-              <span>Telephone</span>
-              <i></i>
-            </div>
-            <div className="inputBox">
-              <select
-                className="select"
-                value={selectedOption}
-                onChange={handleSelect}
-              >
-                <option selected disabled>
-                  Branch...
-                </option>
-                <option value="Badaro">Badaro</option>
-                <option value="Tripoli">Tripoli</option>
-                <option value="Antelias">Antelias</option>
-              </select>
-              <i></i>
-            </div>
-            <br />
-            <ReCAPTCHA
-              id="not_robot"
-              sitekey="6LdrLAwlAAAAAFQkh-PwwW4ngrInomVhXMIIC6Fw"
-              onChange={onChange}
-            />
-            <button type="submit">Submit</button>
-          </div>
-        </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          id="fname"
+          placeholder="First Name"
+          type="text"
+          ref={first_name}
+          required
+        />
+        <br />
+        <input id="lname" placeholder="Last Name" type="text" ref={last_name} />
+        <br />
+        <input
+          id="id"
+          placeholder="Short ID"
+          type="int"
+          pattern=".{6}"
+          ref={short_id}
+          required
+        />
+        <br />
+        <label id="example">example: 123456</label>
+        <br />
+        <input
+          id="uni-mail"
+          placeholder="User ID"
+          type="text"
+          pattern="[a-z]{3}[0-9]{3}"
+          ref={mail}
+          required
+        />
+        <br />
+        <label id="example">example: sam123</label>
+        <br />
+        <input
+          id="phone"
+          placeholder="Telephone"
+          type="int"
+          pattern="[0-9]{2} [0-9]{3} [0-9]{3}||[0-9]{2}-[0-9]{3}-[0-9]{3}||[0-9]{2}[0-9]{3}[0-9]{3}"
+          ref={phone}
+          required
+        />
+        <br />
+        <br />
+        <Select
+          id="branch"
+          placeholder="Branch..."
+          options={branch_options}
+          value={selectedOption}
+          onChange={handleSelect}
+          required
+        />
+        <br />
+        <ReCAPTCHA
+          id="not_robot"
+          sitekey="6LdrLAwlAAAAAFQkh-PwwW4ngrInomVhXMIIC6Fw"
+          onChange={onChange}
+        />
+        <br />
+        <button id="submit" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
